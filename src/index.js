53e91278c6544346256cee52bf39862347baa01a
Irek Yakubov <irekyyakubov@gmail.com>
2022-09-02 15:10:03 +0400

    Класс задачи

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './scss/index.scss';
 import main from './js/main';
+import { newTask } from './js/task';
+
+newTask.plusCounter();
+console.log(newTask);
---
+++ src/js/task.js
@@ -0,0 +1,30 @@
+class Task {
+  #name;
+  #count;
+  #id;
+  constructor(name, count = 0) {
+    this.#name = name;
+    this.#count = count;
+    this.#id = Math.floor(Math.random() * 10e5);
+  }
+
+  plusCounter() {
+    return this.#count += 1;
+  }
+
+  newName(name) {
+    this.#name = name;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  get count() {
+    return this.#count;
+  }
+  get id() {
+    return this.#id;
+  }
+}
+
+export const newTask = new Task('Решить задачу');
